eec16eb1a1bd62896892a3bfd1c116d6f8cbefad
semindan <semindan.dev@gmail.com>
2025-05-04 22:08:18 +0200

    create unified nested config for all pipeline steps

---
+++ src/mushroom/config/config.py
@@ -0,0 +1,47 @@
+from pydantic_settings import BaseSettings
+from pydantic import BaseModel, Field
+
+class FactExtractorSettings(BaseModel):
+    openai_api_key : str | None = None
+    embedding_model : str | None = None
+    chat_model : str | None = None
+    max_tokens : int | str | None = None
+
+class RetrievalSettings(BaseModel):
+    pass
+
+class SpanLabelingSettings(BaseModel):
+    pass
+
+class Settings(BaseSettings):
+    """
+    Load and manage configuration settings for the app and each pipeline step.
+    """
+    fact_extractor: FactExtractorSettings = Field(
+        default_factory=FactExtractorSettings,
+        description="Settings for the fact extractor step."
+    )
+    retrieval: RetrievalSettings = RetrievalSettings(
+        default_factory=RetrievalSettings,
+        description="Settings for the retrieval step."
+    )
+    span_labeling: SpanLabelingSettings = SpanLabelingSettings(
+        default_factory=SpanLabelingSettings,
+        description="Settings for the span labeling step."
+    )
+    
+    class Config:
+        extra = "allow"
+        env_file = ".env.example", ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+        env_nested_delimiter = '__'
+
+    
+settings = Settings()
+
+
+if __name__ == "__main__":
+    print(settings.fact_extractor)
+    print(settings.fact_extractor.openai_api_key)
+    
