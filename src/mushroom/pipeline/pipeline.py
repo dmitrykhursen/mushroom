a825bd489739c94b5f7f345ea222e92361d1d72c
semindan <semindan.dev@gmail.com>
2025-05-06 23:11:32 +0200

    add main entry to pipeline

---
+++ src/mushroom/pipeline/pipeline.py
@@ -0,0 +1,56 @@
+from typing import Any, List, Optional, Union
+
+from mushroom.config import settings
+from mushroom.pipeline.data_connector import read_dataset, write_dataset
+from mushroom.pipeline.interface import Entry
+from mushroom.pipeline.span_labeling.evaluate_span_labeling import \
+    evaluate_span_labeling
+from mushroom.pipeline.span_labeling.span_labeling import \
+    span_labeling_baseline
+
+
+class Pipeline:
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+    
+    def run(self, dataset: Union[List[Entry], str], output_path: Optional[str] = None) -> List[Entry]:
+        if not isinstance(dataset, List):
+            dataset = read_dataset(dataset)
+        
+        processed_dataset = span_labeling_baseline(dataset)
+        
+        
+        if output_path:
+            write_dataset(output_path, processed_dataset)
+        
+        return processed_dataset
+    
+    def evaluate(self, processed_dataset: Union[List[Entry], str]) -> List[Any]:
+        ious, cors = evaluate_span_labeling(processed_dataset)
+        
+        print(f"IOUs: {ious.mean()}")
+        print(f"CORs: {cors.mean()}")
+        
+        return {
+            "ious": ious,
+            "cors": cors
+        }
+        
+            
+if __name__ == "__main__":
+    pipeline = Pipeline()
+    
+    from pathlib import Path
+    project_root = Path(settings.project_root)
+    
+    file_path = project_root / "data/extra/splits/val/v2/mushroom.en-val.v2.extra.jsonl"
+    output_path = project_root / "outputs"
+    
+    output_path.mkdir(parents=True, exist_ok=True)
+    output_file_path = output_path / "predictions.jsonl"
+    
+    predictions = pipeline.run(file_path, output_file_path)
+    results = pipeline.evaluate(output_file_path)
+
+#%%
+        
