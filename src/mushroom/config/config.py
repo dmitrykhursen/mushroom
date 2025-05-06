fd1e48d37e6b241f6a28688123414ebf28aa6247
semindan <semindan.dev@gmail.com>
2025-05-06 23:00:42 +0200

    add project_root + refactor config

--- src/mushroom/config/config.py
+++ src/mushroom/config/config.py
@@ -1,47 +1,52 @@
-from pydantic_settings import BaseSettings
 from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings
+from pathlib import Path
+
 
 class FactExtractorSettings(BaseModel):
-    openai_api_key : str | None = None
-    embedding_model : str | None = None
-    chat_model : str | None = None
-    max_tokens : int | str | None = None
+    openai_api_key: str | None = None
+    embedding_model: str | None = None
+    chat_model: str | None = None
+    max_tokens: int | str | None = None
+
 
 class RetrievalSettings(BaseModel):
     pass
 
+
 class SpanLabelingSettings(BaseModel):
     pass
+
 
 class Settings(BaseSettings):
     """
     Load and manage configuration settings for the app and each pipeline step.
     """
+    
+    project_root: str = Path(__file__).absolute().parent.parent.parent.parent.as_posix()
+
     fact_extractor: FactExtractorSettings = Field(
         default_factory=FactExtractorSettings,
-        description="Settings for the fact extractor step."
+        description="Settings for the fact extractor step.",
     )
     retrieval: RetrievalSettings = RetrievalSettings(
         default_factory=RetrievalSettings,
-        description="Settings for the retrieval step."
+        description="Settings for the retrieval step.",
     )
     span_labeling: SpanLabelingSettings = SpanLabelingSettings(
         default_factory=SpanLabelingSettings,
-        description="Settings for the span labeling step."
+        description="Settings for the span labeling step.",
     )
-    
+
     class Config:
         extra = "allow"
         env_file = ".env.example", ".env"
         env_file_encoding = "utf-8"
         case_sensitive = True
-        env_nested_delimiter = '__'
+        env_nested_delimiter = "__"
 
-    
+
 settings = Settings()
 
-
 if __name__ == "__main__":
-    print(settings.fact_extractor)
-    print(settings.fact_extractor.openai_api_key)
-    
+    print(settings)
